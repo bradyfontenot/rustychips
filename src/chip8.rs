efaafdf0c5ed6b29928ad5898cb13e0ed3dc8f55
Brady Fontenot <bfont52@gmail.com>
2020-09-30 09:27:29 -0500

    build struct impl skeleton

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,17 +1,131 @@
 pub const V_REGISTER_SIZE: usize = 16;
-pub const I_REGISTER_SIZE: usize = 16;
 pub const STACK_SIZE: usize = 16;
 pub const MEMORY_SIZE: usize = 4096;
 pub const FONT_SIZE: usize = 80;
 
-#[derive(Debug, Default)]
-struct Chip8{
-   v_register: [u8; V_REGISTER_SIZE],      // Vx Register
-   i_register: [u16; I_REGISTER_SIZE],     // I Register
-   pc: u16,                                // Program Counter
-   sp: u8,                                 // Stack Pointer
-   stack: [u16; STACK_SIZE],               // Stack
-   memory: [u8; MEMORY_SIZE],
-   font: [u16; FONT_SIZE]
+pub const FONT_SET: [u8; 80] = [
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
 
+
+pub struct Chip8{
+   pub v_register: [u8; V_REGISTER_SIZE],      // Vx Register
+   pub i_register: u16,                        // I Register
+   pub pc: u16,                                // Program Counter
+   pub sp: u8,                                 // Stack Pointer
+   pub stack: [u16; STACK_SIZE],               // Stack
+   pub memory: [u8; MEMORY_SIZE],              // RAM
+  //  font: [u16; FONT_SIZE]
 }
+
+
+impl Chip8 {
+
+  // constructor
+  pub fn init() -> Chip8 {
+    let mut chippy = Chip8{
+      v_register: [0; V_REGISTER_SIZE],
+      i_register: 0x000,
+      pc: 0x200,
+      sp: 0x000,
+      stack: [0; 16],
+      memory: [0; 4096]
+    };
+  
+    Chip8::load_font_set(&mut chippy);
+  
+    chippy
+  }
+
+  // Loads font set into memory
+  // stored in unused portion of memory below 0x200
+  fn load_font_set(chippy: &mut Chip8){
+    for i in 0..FONT_SIZE{
+      chippy.memory[i] = FONT_SET[i];
+    }  
+  }
+
+  // store byte in memory addr
+  pub fn set_memory(mut self, addr: usize, val: u8){
+    self.memory[addr] = val;
+  }
+
+  // get byte from memory addr
+  pub fn get_memory(&self, addr: u16) -> u8 {
+    self.memory[addr as usize]
+  }
+
+  // store byte in V Register
+  // pub fn set_v_byte(){}
+
+  // get byte from V Register
+  // pub fn get_v_byte(){}
+
+  // store byte in I Register
+  // pub fn set_i_byte(){}
+
+  // get byte from I Register
+  // pub fn get_i_byte(){}
+
+  // store addr in PC
+  // pub fn set_pc(){}
+
+  // get addr from PC
+  // pub fn get_pc(){}
+
+  // store addr in SP
+  // pub fn set_sp(){}
+
+  // get addr from sp
+  // pub fn get_sp(){}
+
+  // push instruction to stack
+  // pub fn push_stack(){}
+
+  // get instruction from stack
+  // pub fn pop_stack(){}
+
+  // increment program counter by 2
+  pub fn increment_pc(mut self){
+    self.pc = self.pc + 2;
+  }
+}
+
+
+
+
+
+
+/* 
+set v_register
+get v_register
+set i_register
+get i_register
+ */
+
+//  ******TESTS******
+
+/*
+#[cfg(test)]
+mod tests{
+  #[test]
+  fn test_(){
+    // test here
+  }
+}
+*/
