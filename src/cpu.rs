e089968321119afef0a4a1558a3b9ef882d91c55
Brady Fontenot <bfont52@gmail.com>
2020-10-02 20:57:01 -0500

    Partial Opcode match cases

--- src/cpu.rs
+++ src/cpu.rs
@@ -70,13 +70,64 @@ fn build_opcode(chip8: &mut Chip8) -> Opcode {
   }
 }
 
+pub fn execute(opcode: Opcode){
+  match opcode.p {
+    0x0000 => ,       // pop stack and set pc to popped address.
+
+    0x1000 => chip8.set_pc(opcode.nnn),       // set 
+    0x2000 => chip8.get_memory[opcode.nnn] ,  // push pc to stack, then set pc to nnn
+    0x3000 => if chip8.get_v_reg(opcode.x) == opcode.kk {chip8.incrememnt_pc()},
+    0x4000 => if chip8.get_v_reg(opcode.x) != opcode.kk {chip8.increment_pc()},
+    0x5000 => if chip8.get_v_reg(opcode.x) == chip8.get_v_reg(opcode.y) {chip8.increment_pc()},
+    0x6000 => chip8.set_v_reg(opcode.x, opcode.kk),
+    0x7000 => chip8.set_v_reg(opcode.x, chip8.get_v_reg() + opcode.kk),    // Vx + NN
+    0x8000 => match opcode.n {
+      0x0000 => ,
+      0x0001 => ,
+      0x0002 => ,
+      0x0003 => ,
+      0x0004 => ,
+      0x0005 => ,
+      0x0006 => ,
+      0x0007 => ,
+      0x000E => 
+    },
+    0x9000 => if chip8.get_v_reg(opcode.x) != chip8.get_v_reg(opcode.y) {chip8.increment_pc()},
+    0xA000 => chip8.set_i_reg(opcode.nnn),
+    0xB000 => chip.set_pc(opcode.p + opcode.nnn),
+    0xC000 => println!("Rand TODO"), // Rand
+    0xD000 => println!("Draw TODO"), // Draw
+    0xE000 => match opcode.nn {
+      0x009E => println!("KeyOp TODO"), // Keyop
+      0x00A1 => println!("KeyOp TODO") // Keyop
+    },
+
+    0xF000 => match opcode.nn {
+      0x0007 => println!("Timer TODO"), // LD Vx, DT
+      0x000A => println!("KeyOp TODO"), // LD Vx, K
+      0x0015 => println!("Timer TODO"), // LD DT, Vx
+      0x0018 => println!("Sound TODO"), // LD ST, Vx
+      0x001E => chip8.set_i_reg(chip8.get_i_reg() + opcode.x),  // ADD I, Vx
+      0x0029 => print!("Sprite TODO"), // LD F, Vx
+      0x0033 => println!("BCD TODO"), // LD B, Vx
+      0x0055 => {
+        for i in 0..opcode.x {
+          chip8.set_memory(chip8.get_i_reg() + i, chip8.get_v_reg(i));
+        }
+      },
+      0x0065 => {
+        for i in 0..opcode.x {
+          chip8.set_v_reg(i, chip8.get_memory(chip.get_i_reg() + i));
+        }
+      },
+    },
+
+
+  }
 }
 
 
 /* TODO:
-  grab code from memory at pc counter??
-  build match statement here...
-  send matching op to execute(op)
+set up timers, keyboard map and look at graphics
+write tests for chip8 and cpu.
   */
-
-// pub fn execute(){}
