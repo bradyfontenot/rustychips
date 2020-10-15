f2b477a9a8ae73dee7bbd22600b1aaf173d02829
Brady Fontenot <bfont52@gmail.com>
2020-10-14 22:49:43 -0500

    implement opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,16 +2,19 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;  
 use crate::chip8::Chip8;
+use rand::Rng;
+// use rand::prelude::*;
 
+use crate::display::Display;
 
 pub struct Opcode{
-  code: u16,
-  p: u16,
-  x: u16,
-  y: u16,
-  kk: u16,
-  nnn: u16,
-  n: u16
+  code: u16,    // full opcode
+  p: u16,       // highest bit
+  x: u8,        // low bit of the high byte
+  y: u8,        // high bit of the low byte
+  kk: u8,       // lowest byte
+  nnn: u16,     // lowest 12 bits
+  n: u16        // lowest bit
 }
 
 // load_rom() loads file into memory
@@ -23,14 +26,14 @@ pub fn load_rom(filename: &String,  chip8: &mut Chip8) -> io::Result<()> {
       chip8.set_memory(i, byte.unwrap());
       
       // Debug printout
-      println!("index: {} {:x} | Byte! {:x}", i, i, chip8.get_memory(i));
+      println!("index: {} {:x} | Byte! {:x}", i, i, chip8.memory(i));
       
       i+=1;
   }
 
   // Printing for debug purposes
-  println!("memory 512 {:x} {:x}", chip8.get_memory(0x200), chip8.get_memory(512));
-  println!("Font Set: {:x}", chip8.get_memory(0));
+  println!("memory 512 {:x} {:x}", chip8.memory(0x200), chip8.memory(512));
+  println!("Font Set: {:x}", chip8.memory(0));
   Ok(())
 }
 
@@ -39,7 +42,7 @@ pub fn read_opcode(chip8: &mut Chip8) -> Opcode {
   let opcode = build_opcode(chip8);
   
   // increment pc by 2
-  chip8.increment_pc();
+  chip8.pc_plus_2();
 
   println!("Opcode: {:x}", opcode.code);
   println!("Opcode.p: {:x}", opcode.p);
@@ -55,74 +58,133 @@ pub fn read_opcode(chip8: &mut Chip8) -> Opcode {
 // build_opcode() - opcodes are 2 bytes but bytes are read from memory one at a time
 // so byte pairs in sequence need to be combined to build a full opcode
 fn build_opcode(chip8: &mut Chip8) -> Opcode {
-  let hi_byte: u8 = chip8.get_memory(chip8.get_pc());
-  let low_byte: u8 = chip8.get_memory(chip8.get_pc() + 1);
+  let hi_byte: u8 = chip8.memory(chip8.get_pc());
+  let low_byte: u8 = chip8.memory(chip8.get_pc() + 1);
   let opcode: u16 = (hi_byte as u16 * 0x100) + (low_byte as u16);
 
   Opcode{
     code: opcode,
     p:    opcode & 0xF000,
-    x:    opcode & 0x0F00,
-    y:    opcode & 0x00F0,
-    kk:   opcode & 0xFF,
+    x:    (opcode & 0x0F) as u8,
+    y:    (opcode & 0x00F) as u8,
+    kk:   (opcode & 0xFF) as u8,
     nnn:  opcode & 0xFFF,
     n:    opcode & 0xF
   }
 }
 
-pub fn execute(opcode: Opcode){
+pub fn execute(opcode: Opcode, chip8: &mut Chip8, display: &mut Display){
   match opcode.p {
-    0x0000 => ,       // pop stack and set pc to popped address.
-
+    0x0000 => match opcode.kk {
+      0x00E0 => display.canvas.clear(),   // println!("0x00E0 Not Handled. TODO: Clear Screen."),
+      0x00EE => {
+        let addr = chip8.pop_stack();
+        chip8.set_pc(addr)
+      },
+      _ => println!("0x000 Not Handled")
+    },
     0x1000 => chip8.set_pc(opcode.nnn),       // set 
-    0x2000 => chip8.get_memory[opcode.nnn] ,  // push pc to stack, then set pc to nnn
-    0x3000 => if chip8.get_v_reg(opcode.x) == opcode.kk {chip8.incrememnt_pc()},
-    0x4000 => if chip8.get_v_reg(opcode.x) != opcode.kk {chip8.increment_pc()},
-    0x5000 => if chip8.get_v_reg(opcode.x) == chip8.get_v_reg(opcode.y) {chip8.increment_pc()},
+    0x2000 => {
+      chip8.push_stack(chip8.get_pc());
+      chip8.set_pc(opcode.nnn);
+    },
+    0x3000 => if chip8.v_register(opcode.x) == opcode.kk {chip8.pc_plus_2()},
+    0x4000 => if chip8.v_register(opcode.x) != opcode.kk {chip8.pc_plus_2()},
+    0x5000 => if chip8.v_register(opcode.x) == chip8.v_register(opcode.y) {chip8.pc_plus_2()},
     0x6000 => chip8.set_v_reg(opcode.x, opcode.kk),
-    0x7000 => chip8.set_v_reg(opcode.x, chip8.get_v_reg() + opcode.kk),    // Vx + NN
+    0x7000 => chip8.set_v_reg(opcode.x, chip8.v_register(opcode.x) + opcode.kk),    // Vx + NN
     0x8000 => match opcode.n {
-      0x0000 => ,
-      0x0001 => ,
-      0x0002 => ,
-      0x0003 => ,
-      0x0004 => ,
-      0x0005 => ,
-      0x0006 => ,
-      0x0007 => ,
-      0x000E => 
+      0x0000 => chip8.set_v_reg(opcode.x, opcode.y as u8),
+      0x0001 => chip8.set_v_reg(opcode.x, (opcode.x | opcode.y) as u8),
+      0x0002 => chip8.set_v_reg(opcode.x, (opcode.x & opcode.y) as u8),
+      0x0003 => chip8.set_v_reg(opcode.x, (opcode.x ^ opcode.y) as u8),
+      0x0004 => match (chip8.v_register(opcode.x)).overflowing_add(chip8.v_register(opcode.y)){
+        (val, true) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 1);
+        },
+        (val, false) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 0);
+        }
+      },
+      0x0005 => match (chip8.v_register(opcode.x)).overflowing_sub(chip8.v_register(opcode.y)){
+        (val, true) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 0);
+        },
+        (val, false) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 1);
+        },
+      },
+      0x0006 => {
+        chip8.set_v_reg(0xF, chip8.v_register(opcode.x) & 0xF);
+        chip8.set_v_reg(opcode.x, chip8.v_register(opcode.x) >> 1);
+      }, 
+      0x0007 => match (chip8.v_register(opcode.y)).overflowing_sub(chip8.v_register(opcode.x)){
+        (val, true) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 0);
+        },
+        (val, false) => {chip8.set_v_reg(opcode.x, val); 
+          chip8.set_v_reg(0xF, 1);
+        }
+      },
+      0x000E => { // TODO: RESARCH MORE! NOT FINAL
+        chip8.set_v_reg(0xF, chip8.v_register(opcode.x) & 0xF0);
+        chip8.set_v_reg(opcode.x, chip8.v_register(opcode.x) << 1);
+      },
+      _ => println!("0x8000 Not Handled")
     },
-    0x9000 => if chip8.get_v_reg(opcode.x) != chip8.get_v_reg(opcode.y) {chip8.increment_pc()},
+    0x9000 => if chip8.v_register(opcode.x) != chip8.v_register(opcode.y) {chip8.pc_plus_2()},
     0xA000 => chip8.set_i_reg(opcode.nnn),
-    0xB000 => chip.set_pc(opcode.p + opcode.nnn),
-    0xC000 => println!("Rand TODO"), // Rand
-    0xD000 => println!("Draw TODO"), // Draw
-    0xE000 => match opcode.nn {
-      0x009E => println!("KeyOp TODO"), // Keyop
-      0x00A1 => println!("KeyOp TODO") // Keyop
+    0xB000 => chip8.set_pc(opcode.p + opcode.nnn),
+    0xC000 => {
+      let mut rng = rand::thread_rng();
+      chip8.set_v_reg(opcode.x, rng.gen::<u8>() & opcode.kk)
     },
+    0xD000 => {
+      let addr = chip8.i_register();
+      let mut y_coord: usize ;
+      let mut x_coord: usize;
+      chip8.set_v_reg(0xF, 0);
+      for y in 0..opcode.n {
+        y_coord = ((chip8.v_register(opcode.y) as u16 + y) % 32) as usize;
+        let sprite = chip8.memory(addr + y);
+        for x in 0..8{
+          x_coord = ((chip8.v_register(opcode.x) as u16 + x) % 64) as usize;
+          let val = display.pixels[y_coord][x_coord] & ((sprite >> (7-x)) & 1);
+          chip8.set_v_reg(0xF, val);
+          display.pixels[y_coord][x_coord] ^= (sprite >> 7-x) & 1;
 
-    0xF000 => match opcode.nn {
-      0x0007 => println!("Timer TODO"), // LD Vx, DT
+        }
+
+      }
+    },
+    0xE000 => match opcode.kk {
+      0x009E => println!("KeyOp TODO"), // Keyop
+      0x00A1 => println!("KeyOp TODO"), // Keyop
+      _ => println!("0xE000 Not Handled")
+    },
+    0xF000 => match opcode.kk {
+      0x0007 => chip8.set_v_reg(opcode.x, chip8.delay_timer() as u8),
       0x000A => println!("KeyOp TODO"), // LD Vx, K
-      0x0015 => println!("Timer TODO"), // LD DT, Vx
-      0x0018 => println!("Sound TODO"), // LD ST, Vx
-      0x001E => chip8.set_i_reg(chip8.get_i_reg() + opcode.x),  // ADD I, Vx
-      0x0029 => print!("Sprite TODO"), // LD F, Vx
-      0x0033 => println!("BCD TODO"), // LD B, Vx
+      0x0015 => chip8.set_delay_timer(chip8.v_register(opcode.x)), // LD DT, Vx
+      0x0018 => chip8.set_sound_timer(chip8.v_register(opcode.x)), // LD ST, Vx
+      0x001E => chip8.set_i_reg(chip8.i_register() + opcode.x as u16),  // ADD I, Vx
+      0x0029 => chip8.set_i_reg(chip8.fonts[chip8.v_register(opcode.x) as usize] as u16),
+      0x0033 => {
+        
+      }, // LD B, Vx
       0x0055 => {
-        for i in 0..opcode.x {
-          chip8.set_memory(chip8.get_i_reg() + i, chip8.get_v_reg(i));
+        for i in 0..opcode.x + 1 {
+          chip8.set_memory(chip8.i_register() + i as u16, chip8.v_register(i));
         }
       },
       0x0065 => {
-        for i in 0..opcode.x {
-          chip8.set_v_reg(i, chip8.get_memory(chip.get_i_reg() + i));
+        for i in 0..opcode.x+ 1 {
+          chip8.set_v_reg(i, chip8.memory(chip8.i_register() + i as u16));
         }
       },
+      _ => println!("0xF000 Not Handled")
     },
-
-
+    _ => println!("Opcode Not Handled")
   }
 }
 
